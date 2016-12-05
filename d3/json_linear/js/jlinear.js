b2c7e6c5d864b2879d6208ad8086ee15423bf460
森川綾香 <AyakaMorikawa@kenshuuyoupasokon-no-MacBook-Air.local>
2016-12-05 18:45:12 +0900

    棒グラフの描画方向の変更

--- d3/json_linear/js/jlinear.js
+++ d3/json_linear/js/jlinear.js
@@ -1,41 +1,50 @@
-//var color = "blue";
-var height = 200;
+var xMargin = 50;
 
 var svg = d3.select("#result").append("svg")
-    .attr('width', 200)
+    .attr('width', 300)
     .attr('height', 200);
-/*
-//横方向の棒グラフ
-    xScale = d3.scale.linear().domain([0,100]).range([0,200]);
-    yScale = d3.scale.linear().domain([0,5]).range([0,100]);
-*/
-//縦方向の棒グラフ
-//rangeの数字を変えるとグラフの幅が変わる
-yScale = d3.scale.linear().domain([100,0]).range([200,0]);
-xScale = d3.scale.linear().domain([0,5]).range([0,120]);
 
 d3.json("./jlinear.json", function(error, data) {
-  /*
-  //横方向の棒グラフ
+
+    //縦の棒グラフ
+    scale = d3.scale.linear()
+        .domain([0, d3.max(data, function(d) {
+            return d.value
+        })])
+        .range([200, 0]);
+
+    //縦方向の棒グラフ
     svg.selectAll("rect")
         .data(data)
         .enter()
         .append("rect")
-        .attr('x', 0)
-        .attr('y', function(d,i) { return yScale(i);})
-        .attr('height', 18)
-        .attr('width', function(d){ return xScale(d.value);})
-        .attr('fill', 'blue');
-*/
-//縦方向の棒グラフ
-//上方向から下方向へ
-svg.selectAll("rect")
-.data(data)
-.enter()
-.append("rect")
-.attr("x",function(d,i){ return xScale(i);})
-.attr("y",0)
-.attr("height",function(d){ return yScale(d.value);})
-.attr("width",18)
-.attr('fill', 'red');
+        .attr("x", function(d, i) {
+            return i * 30 + xMargin;
+        })
+        .attr("y", function(d) {
+            return scale(d.value)
+        })
+        .attr("height", function(d) {
+            return 200 - scale(d.value);
+        })
+        .attr("width", 20)
+        .attr('fill', 'red');
+
+    /*
+    //横の棒グラフ
+    scale = d3.scale.linear()
+        .domain([0,d3.max(data,function(d){ return d.value})])
+        .range([0,200]);
+
+        svg.selectAll("rect")
+            .data(data)
+            .enter()
+            .append("rect")
+            .attr('x', 0)
+            .attr('y', function(d,i) { return i * 30})
+            .attr('height', 20)
+            .attr('width', function(d){ return scale(d.value);})
+            .attr('fill', 'blue');
+            */
+
 });
