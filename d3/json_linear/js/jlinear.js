6c899fd1bb1c2cf6e72bbbd71db972445c200a62
森川綾香 <AyakaMorikawa@kenshuuyoupasokon-no-MacBook-Air.local>
2016-12-14 18:51:26 +0900

    x軸y軸の作成

--- d3/json_linear/js/jlinear.js
+++ d3/json_linear/js/jlinear.js
@@ -1,34 +1,25 @@
-var xMargin = 50;
+var margin = {top: 20, right: 20, bottom: 30, left: 50};
+var width = 300;
+var height = 200; 
+var bwidth = 30;　//棒グラフの幅
 
-var svg = d3.select("#result").append("svg")
-    .attr('width', 300)
-    .attr('height', 200);
-
+var svg = d3.select("#result").append("svg")　//div内にsvg要素を追加
+    .attr('width', 360)
+    .attr('height', 250);
+    
+//jsonデータの読み込み
 d3.json("./jlinear.json", function(error, data) {
+	
+		var x = d3.scale.ordinal()
+        .domain(data.map(function (d) { 
+	          return d.name; }))
+        .rangeRoundPoints([0, width], 1);
 
-    //縦の棒グラフ
-    scale = d3.scale.linear()
-        .domain([0, d3.max(data, function(d) {
-            return d.value
+    var y = d3.scale.linear() //linear = 線形
+        .domain([0, d3.max(data, function(d) {　//入力データの範囲をデータの最大値を利用
+            return d.value;                      //表示上のサイズ
         })])
-        .range([200, 0]);
-
-    //縦方向の棒グラフ
-    svg.selectAll("rect")
-        .data(data)
-        .enter()
-        .append("rect")
-        .attr("x", function(d, i) {
-            return i * 30 + xMargin;
-        })
-        .attr("y", function(d) {
-            return scale(d.value)
-        })
-        .attr("height", function(d) {
-            return 200 - scale(d.value);
-        })
-        .attr("width", 20)
-        .attr('fill', 'red');
+        .range([height, 0]);                       //実際の出力サイズ
 
     /*
     //横の棒グラフ
@@ -46,5 +37,48 @@ d3.json("./jlinear.json", function(error, data) {
             .attr('width', function(d){ return scale(d.value);})
             .attr('fill', 'blue');
             */
+            
+    //x軸の表示        
+    var xAxis = d3.svg.axis()
+    		.scale(x)
+        .orient('bottom');
+        
+    //y軸の表示
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient('left'); //目盛をグラフのどこに表示するか
+
+    svg.append('g')
+        .attr({
+	        class: 'x axis',
+          transform: "translate("+ margin.left + "," + height + ")"  //目盛がきれないようにmargin.leftずらしている
+          })
+        .call(xAxis);
+			
+    svg.append("g")
+        .attr({
+           class: "y axis",
+           transform: "translate(" + margin.left + ",0)" //transform内の値を変更することで
+        })                                        　　　　//目盛の位置を変更できる
+        .call(yAxis);
+        
+    //縦方向の棒グラフ
+    svg.selectAll("rect")
+        .data(data)
+        .enter()
+        .append("rect") //rect要素の追加
+        .attr("x", function(d) {
+            return x(d.name) + margin.left - bwidth/2;　//棒の位置を決める(margin.left-bwidth/2で調整)
+        })																							//軸をmargin.left分だけずらしているため
+        .attr("y", function(d) {
+            return y(d.value);
+        })
+        .attr("height", function(d) {
+            return height - y(d.value);　	
+        })
+        .attr("width", bwidth)
+        .attr('fill', 'red');
+
+
 
 });
